c6d9bf9febfdb9957ca4d14ec693d06eabe105a5
DT <759431507@qq.com>
2023-07-12 15:24:32 +0800

    添加sql 175

---
+++ java/src/leetcode/editor/cn/[176]第二高的薪水.sql
@@ -0,0 +1,78 @@
+package leetcode.editor.cn;
+
+#Employee 表：
+#
+# 
+# 
+# 
+#+-------------+------+
+#| Column Name | Type |
+#+-------------+------+
+#| id          | int  |
+#| salary      | int  |
+#+-------------+------+
+#id 是这个表的主键。
+#表的每一行包含员工的工资信息。
+# 
+# 
+# 
+#
+# 
+#
+# 编写一个 SQL 查询，获取并返回 Employee 表中第二高的薪水 。如果不存在第二高的薪水，查询应该返回 null 。 
+#
+# 查询结果如下例所示。 
+#
+# 
+#
+# 示例 1： 
+#
+# 
+#输入：
+#Employee 表：
+#+----+--------+
+#| id | salary |
+#+----+--------+
+#| 1  | 100    |
+#| 2  | 200    |
+#| 3  | 300    |
+#+----+--------+
+#输出：
+#+---------------------+
+#| SecondHighestSalary |
+#+---------------------+
+#| 200                 |
+#+---------------------+
+# 
+#
+# 示例 2： 
+#
+# 
+#输入：
+#Employee 表：
+#+----+--------+
+#| id | salary |
+#+----+--------+
+#| 1  | 100    |
+#+----+--------+
+#输出：
+#+---------------------+
+#| SecondHighestSalary |
+#+---------------------+
+#| null                |
+#+---------------------+
+# 
+#
+#
+# 👍 1355 👎 0
+
+public class SecondHighestSalary{
+	public static void main(String[] args) {
+		Solution solution = new SecondHighestSalary().new Solution();
+		
+	}
+#leetcode submit region begin(Prohibit modification and deletion)
+# Write your MySQL query statement below
+#leetcode submit region end(Prohibit modification and deletion)
+
+}
