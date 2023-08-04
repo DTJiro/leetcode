613c2d0ac91966281b60370020c9cdd848eedcca
DT <759431507@qq.com>
2023-08-04 16:14:44 +0800

    VIP 1194 题目、解答看及代码

---
+++ java/src/leetcode/editor/cn/[1194]锦标赛优胜者.sql
@@ -0,0 +1,106 @@
+package leetcode.editor.cn;
+
+#Players 玩家表 
+#
+# 
+#+-------------+-------+
+#| Column Name | Type  |
+#+-------------+-------+
+#| player_id   | int   |
+#| group_id    | int   |
+#+-------------+-------+
+#player_id 是此表的主键。
+#此表的每一行表示每个玩家的组。
+# 
+#
+# Matches 赛事表 
+#
+# 
+#+---------------+---------+
+#| Column Name   | Type    |
+#+---------------+---------+
+#| match_id      | int     |
+#| first_player  | int     |
+#| second_player | int     | 
+#| first_score   | int     |
+#| second_score  | int     |
+#+---------------+---------+
+#match_id 是此表的主键。
+#每一行是一场比赛的记录，first_player 和 second_player 表示该场比赛的球员 ID。
+#first_score 和 second_score 分别表示 first_player 和 second_player 的得分。
+#你可以假设，在每一场比赛中，球员都属于同一组。
+# 
+#
+# 
+#
+# 每组的获胜者是在组内累积得分最高的选手。如果平局，player_id 最小 的选手获胜。 
+#
+# 编写一个 SQL 查询来查找每组中的获胜者。 
+#
+# 返回的结果表单 没有顺序要求 。 
+#
+# 查询结果格式如下所示。 
+#
+# 
+#
+# 示例 1: 
+#
+# 
+#输入：
+#Players 表:
+#+-----------+------------+
+#| player_id | group_id   |
+#+-----------+------------+
+#| 15        | 1          |
+#| 25        | 1          |
+#| 30        | 1          |
+#| 45        | 1          |
+#| 10        | 2          |
+#| 35        | 2          |
+#| 50        | 2          |
+#| 20        | 3          |
+#| 40        | 3          |
+#+-----------+------------+
+#Matches 表:
+#+------------+--------------+---------------+-------------+--------------+
+#| match_id   | first_player | second_player | first_score | second_score |
+#+------------+--------------+---------------+-------------+--------------+
+#| 1          | 15           | 45            | 3           | 0            |
+#| 2          | 30           | 25            | 1           | 2            |
+#| 3          | 30           | 15            | 2           | 0            |
+#| 4          | 40           | 20            | 5           | 2            |
+#| 5          | 35           | 50            | 1           | 1            |
+#+------------+--------------+---------------+-------------+--------------+
+#输出：
+#+-----------+------------+
+#| group_id  | player_id  |
+#+-----------+------------+ 
+#| 1         | 15         |
+#| 2         | 35         |
+#| 3         | 40         |
+#+-----------+------------+ 
+#
+# Related Topics 数据库 👍 26 👎 0
+
+public class TournamentWinners{
+	public static void main(String[] args) {
+		Solution solution = new TournamentWinners().new Solution();
+		
+	}
+#leetcode submit region begin(Prohibit modification and deletion)
+# Write your MySQL query statement below
+
+select group_id,player player_id from
+(select b.group_id,e.player,dense_rank() over(partition by group_id order by score desc,e.player) rn from (
+        select player,sum(score) score from (
+            select first_player player,first_score score from Matches
+            union all
+            select second_player player,second_score score from Matches
+        ) a group by a.player
+    ) e left join Players b
+    on b.player_id=e.player
+) d where d.rn=1
+
+#leetcode submit region end(Prohibit modification and deletion)
+
+}
